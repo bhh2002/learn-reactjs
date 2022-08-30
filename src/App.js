523e99929021e14428168a785bc8f9cca042d88a
Touliver <hoangteen196@gmail.com>
2022-08-30 20:39:56 +0700

    update 30-8

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
 function App() {
+  const name = "Hoang";
+  const age = 18;
+  const isMale = true;
+  const student = {
+    name: "Easy Frontend",
+  };
+  const colorList = ["red", "green", "blue"];
+  console.log(colorList[1]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Bui Huy Hoang</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here
-        </a>
+        <p>
+          Xin chào {name} - {age} - {isMale ? "Male" : "Female"}
+        </p>
+        {/* Hàm if - else */}
+        {/* Dạng 1: */}
+        {isMale ? <p>Nam</p> : <p>Nu</p>}
+
+        {/* Dạng 2 tầng */}
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+        {/* Hàm if - else dùng <React.Fragment></React.Fragment> làm thẻ cha */}
+        {isMale && (
+          <React.Fragment>
+            <p>Male1</p>
+            <p>Male2</p>
+            <p>Male3</p>
+          </React.Fragment>
+        )}
+
+        {/* Dạng rút gọn của <React.Fragment></React.Fragment> */}
+        {isMale && (
+          <>
+            <p>Male1</p>
+            <p>Male2</p>
+            <p>Male3</p>
+          </>
+        )}
+
+        {/* Render các biến trong mảng */}
+        <p>{student.name}</p>
+
+        {/* Render các thành phần trong mảng */}
+        <ul>
+          {colorList.map((color) => (
+            <li key={color} style={{ color }}>
+              {color}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
