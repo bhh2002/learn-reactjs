f68ba0f00118fd8c8793ef307b7f10e4c380d02c
Touliver <hoangteen196@gmail.com>
2022-08-30 09:29:57 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Bui Huy Hoang</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hoang Bui
+          Click here
         </a>
       </header>
     </div>
